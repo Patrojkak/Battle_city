a691a66a7458fae80368ac733bd4df699b367cd4
Patrojkak <153901794+Patrojkak@users.noreply.github.com>
2024-11-21 11:26:21 +0100

    Add files via upload
    menu

---
+++ lvl1scena.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class lvl1scena : MonoBehaviour
+{
+    public void lvlOneScena()
+    {
+        SceneManager.LoadScene("LVL1");
+    }
+
+}
